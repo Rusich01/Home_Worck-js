6683677a5ed6211bc7c6ccdb9d393e0bf26b3be2
Ruslan Hrytsai <gritsay9457@gmail.com>
2023-06-28 23:38:32 +0300

    added the solution

--- scr/script05.js
+++ scr/script05.js
@@ -1,6 +1,10 @@
 // Усічення рядка
-
 const str = prompt('Enter your string');
 const index = prompt('Enter your index number');
 const lengthStr = str.slice(0, index);
-console.log(lengthStr);
+
+if (str.length - 1 > index) {
+  console.log(lengthStr);
+} else {
+  console.log(`${str}................`);
+}
