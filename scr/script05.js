4e9f8f03122066604c48b2137e3eca2b2b0db734
Ruslan Hrytsai <gritsay9457@gmail.com>
2023-07-01 22:47:08 +0300

    file js.6

--- scr/script05.js
+++ scr/script05.js
@@ -1,10 +1,17 @@
 // Усічення рядка
-const str = prompt('Enter your string');
-const index = prompt('Enter your index number');
-const lengthStr = str.slice(0, index);
+// const str = prompt('Enter your string');
+// const index = prompt('Enter your index number');
 
-if (str.length - 1 > index) {
-  console.log(lengthStr);
-} else {
-  console.log(`${str}................`);
-}
+// function strings(str, index) {
+//   const lengthStr = str.slice(0, index);
+
+//   if (str.length - 1 > index) {
+//     console.log(`${lengthStr}...`);
+//   } else {
+//     console.log(str);
+//   }
+// }
+
+
+
+
---
+++ scr/script06.js
@@ -0,0 +1,55 @@
+// Функція: maxNumber;
+
+// const firstNumber = prompt('давай перше число ');
+// const secondNumber = prompt('давай друге число ');
+
+// function maxNumber(a, b) {
+//   const max = Math.max(firstNumber, secondNumber);
+//   console.log(max);
+// }
+
+// maxNumber(firstNumber, secondNumber);
+
+//   Функція:  checkAge !
+
+// const age = prompt('Введіть вік');
+// function checkAge(age) {
+//   if (age >= 18) {
+//     console.log(true);
+//   } else {s
+//     console.log(false);
+//   }
+// }
+// checkAge(age);
+
+// Функція: усічення рядка
+
+// const str = prompt('Enter your string');
+// const index = prompt('Enter your index number');
+
+// function strings(str, index) {
+//   const lengthStr = str.slice(0, index);
+
+//   if (str.length > index) {
+//     console.log(`${lengthStr}...`);
+//   } else {
+//     console.log(str);
+//   }
+// }
+// strings(str, index);
+
+//  Функція; checkWord
+
+// const str = prompt('давай рядоок ').toLocaleLowerCase();
+// const world = prompt('давай слово ').toLocaleLowerCase();
+
+// function checkWord(str, world) {
+//   if (str.includes(world)) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// checkWord(str, world);
+
+// Влад є питання, чому в останній функції якщо залишити обидва поля пустими,  то все одно  буде true, це ж пустий рядок начебто
