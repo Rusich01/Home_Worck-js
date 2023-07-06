5da3dbbafc6624bd16e35c480ed824e52e33529f
Ruslan Hrytsai <gritsay9457@gmail.com>
2023-07-06 21:18:49 +0300

    ...

--- scr/script06.js
+++ scr/script06.js
@@ -3,23 +3,24 @@
 // const firstNumber = prompt('давай перше число ');
 // const secondNumber = prompt('давай друге число ');
 
-// const  maxNumber = (a, b) => {
-//   const max = Math.max(a, b);
-//   console.log(max);
+// const maxNumber = (a, b) => {
+//     const max = Math.max(a, b);
+//     console.log(max);
 // }
 
 // maxNumber(firstNumber, secondNumber);
 
-//   Функція:  checkAge !
+// Функція: checkAge!
 
 // const age = prompt('Введіть вік');
 
-// const  checkAge = (age) => {
-//   if (age >= 18) {
-//     console.log(true);
-//   } else {s
-//     console.log(false);
-//   }
+// const checkAge = (age) => {
+//     if (age >= 18) {
+//         console.log(true);
+//     } else {
+//         s
+//         console.log(false);
+//     }
 // }
 // checkAge(age);
 
@@ -32,23 +33,23 @@
 // const strings = (strng, indexString) => {
 
 //     const lengthStr = strng.slice(0, indexString);
-    
-// (strng.length > indexString)?  console.log(`${lengthStr}...`) :    console.log(strng);
+
+//     (strng.length > indexString) ? console.log(`${lengthStr}...`) : console.log(strng);
 
 // }
 // strings(str, index);
 
-//  Функція; checkWord
+// Функція; checkWord
 
 // const str = prompt('давай рядоок ').toLocaleLowerCase();
 // const world = prompt('давай слово ').toLocaleLowerCase();
 
-// const  checkWord =(str, world) => {
-//   if (str.includes(world)) {
-//     console.log(true);
-//   } else {
-//     console.log(false);
-//   }
+// const checkWord = (str, world) => {
+//     if (str.includes(world)) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
 // }
 // checkWord(str, world);
 
