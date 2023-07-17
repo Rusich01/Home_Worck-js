1ca9a32ecf52bc273de8c18dcaa88e1b09d7fd2c
Ruslan Hrytsai <gritsay9457@gmail.com>
2023-07-17 22:38:33 +0300

    HomeWorch№ 6/7

---
+++ scr/script07.js
@@ -0,0 +1,26 @@
+const array = ['lenovo', 'dell', 'apple', 'asus', 'acer'];
+const arrayOfUsers = ['user', 'user1', 'user10', 'user100', 'user1000'];
+
+// Завдання № 1
+// function audit(item) {
+//   console.log(item, item.length);
+// }
+// array.forEach(audit);
+
+// Скороченний  запис
+// array.forEach(item => console.log(item, item.length));
+
+// Завдання № 2
+// const giveWord = prompt('Який бред обиреш?').toLowerCase();
+
+// const search = (a, b) => {
+//   return console.log(a.includes(b));
+// };
+
+// search(array, giveWord);
+
+// Завдання № 3
+// console.log(arrayOfUsers.find(item => item.length > 4));
+
+// Завдання № 4
+// console.log(arrayOfUsers.filter(item => item.length > 4));
---
+++ scr/script08.js
@@ -0,0 +1,18 @@
+const users = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6'];
+
+// Завдання №1
+// Вирішив зробити через forEach:
+
+// console.log(
+//   users.forEach(item =>
+//     item.length >= 4 ? console.log(item, true) : console.log(item, false),
+//   ),
+// );
+
+// console.log(users.every(item => item.length > 4));
+
+// Завдання №2
+// console.log(users.some(item => item.length > 4));
+
+// Завдання №3
+// console.log(users.map(item => item.toUpperCase(item)));
